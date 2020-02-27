a0207d8a40fbde9daab6b950d71de380783a2d34
abelikov5 <abelikov@inbox.ru>
2020-02-27 11:34:57 +0300

    ok for today

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-var target = document.getElementById("target");
-var btn = document.getElementById("btn_one");
-
-let arr = {};
+var btn = document.getElementById("btn_one"); 
+var text = document.getElementById("input_text");
+var i = 0;
 
 btn.addEventListener('click', function(e) {
   e.preventDefault();
-  var text = document.getElementById("input_text");
-  // arr[0] = text.value;
-  // console.log(arr); 
+  i++;
+  var target = document.getElementById("str_" + i);
+
+  console.log("str_" + i); 
+  if (i > 9) {
+    window.location.reload();
+  }
   target.innerHTML = text.value;
+  document.getElementById('input_text').value = '';
+
 });
 
