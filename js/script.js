d68dbf6dfd2ee021639903d74e8fae3fac6cd748
abelikov5 <48707335+abelikov5@users.noreply.github.com>
2020-03-08 16:23:54 +0300

    new block added on bottom win, fix some media bugs

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 var btn = document.getElementById("btn_one"); 
 var text = document.getElementById("input_text");
-var i = 0;
+// var i = 0;
 
 btn.addEventListener('click', function(e) {
   e.preventDefault();
-  i++;
-  var target = document.getElementById("str_" + i);
+  // i++;
+  // var target = document.getElementById("str_" + i);
+
+  let newCom = document.createElement("div");
+  newCom.classList.add("str_div");
+  newCom.innerHTML = text.value;
+  document.getElementById("win_com").appendChild(newCom);
 
   console.log(text.value); 
-  if (text.value == 'clear') {
+  if (text.value == '/clear') {
     window.location.reload();
     
   }
-  target.innerHTML = text.value;
   document.getElementById('input_text').value = '';
 
 });
---
+++ script_test.js
@@ -0,0 +1,11 @@
+let div_test = document.createElement("div");
+div_test.classList.add("help_div");
+// div_test.textContent = "test";
+
+document.getElementById("help").appendChild(div_test);
+
+
+// var d = document.createElement("div");
+
+// document.getElementById("help").appendChild(d);
+// d.classList.add("help-div");
